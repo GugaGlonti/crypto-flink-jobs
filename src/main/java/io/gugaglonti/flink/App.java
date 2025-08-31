3fc9f5596768302dbfab0485eb30f268dd44d4e8
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2025-08-31 22:30:33 +0200

    initial commit

---
+++ src/main/java/io/gugaglonti/flink/App.java
@@ -0,0 +1,13 @@
+package io.gugaglonti.flink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

2169ba319e90f2b0f5e73642eb632bd242523ac9
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2025-08-31 23:48:04 +0200

    add: minimal flink job

--- src/main/java/io/gugaglonti/flink/App.java
+++ src/main/java/io/gugaglonti/flink/App.java
@@ -1,13 +1,30 @@
 package io.gugaglonti.flink;
 
+import java.util.List;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<String> text = env.fromData(List.of("Hello", "World"));
+
+        text.map((MapFunction<String, String>) value -> value.toUpperCase());
+
+        text.print();
+
+        try {
+            env.execute("Flink Job Test");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

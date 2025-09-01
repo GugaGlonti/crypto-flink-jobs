183cdd7cc77ac3bd695e84312fb1f5e3c8919559
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2025-09-02 01:35:34 +0200

    refactor: upload script

--- scripts/upload-job.sh
+++ scripts/upload-job.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+JAR_FILE_NAME=crypto-flink-jobs-1.0-SNAPSHOT.jar
+JAR_FILE=target/$JAR_FILE_NAME
+
 curl -X POST http://localhost:8081/jars/upload \
-  -F "jarfile=@target/crypto-flink-jobs-1.0-SNAPSHOT.jar"
+  -F "jarfile=@$JAR_FILE"
